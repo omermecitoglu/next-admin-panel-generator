6eeaf9167794429d842059fc02e5bddc92c7677c
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-09 13:41:49 +0700

    feat: improved actions

--- src/templates/actions.ts
+++ src/templates/actions.ts
@@ -1,16 +1,22 @@
-import { kebabCase } from "~/core/string";
+import { camelCase, kebabCase, pascalCase } from "~/core/string";
 import getTemplate from "~/core/template";
 import handleBarsTemplate from "./actions.hbs";
 
 type ActionsTemplate = {
   i18n: boolean,
+  camelCaseSingular: string,
   kebabCasePlural: string,
+  kebabCaseSingular: string,
+  pascalCasePlural: string,
 };
 
 export default function generateActions(modelName: string, i18n: boolean) {
   const template = getTemplate<ActionsTemplate>(handleBarsTemplate);
   return template({
     i18n,
+    camelCaseSingular: camelCase(modelName, false),
     kebabCasePlural: kebabCase(modelName, true),
+    kebabCaseSingular: kebabCase(modelName, false),
+    pascalCasePlural: pascalCase(modelName, false),
   });
 }
